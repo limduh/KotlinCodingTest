eb7e7a96c7ea74b9d677b9b7f76dfde34ac936ad
limduhyoung <limduh2@naver.com>
2023-12-17 23:25:36 +0900

    2번째 코딩

---
+++ KotlinCoding/Kotlincoding2.kt
@@ -0,0 +1,17 @@
+package KotlinCoding
+
+//정수 a와 b가 주어집니다. 각 수를 입력받아 입출력 예와 같은 형식으로 출력하는 코드를 작성해 보세요.
+//입력
+//
+//4 5
+//출력
+//
+//a = 4
+//b = 5
+
+fun main(args: Array<String>) {
+    val (a, b) = readln().split(' ').map(String::toInt)
+
+    println("a = $a")
+    println("b = $b")
+}
--- kotlincoding.kt
+++
@@ -1,9 +0,0 @@
-class kotlincoding {
-}
-//문제 설명
-//문자열 str이 주어질 때, str을 출력하는 코드를 작성해 보세요.
-
-fun main(args: Array<String>) {
-    val str = readLine()!!
-    print(str)
-}
