6db1e038224fdafe105d1a4b4e1cdc8aed1623cc
limduhyoung <limduh2@naver.com>
2023-12-18 23:41:26 +0900

    coding3

---
+++ KotlinCoding/KotlinCoding3.kt
@@ -0,0 +1,20 @@
+package KotlinCoding
+//
+//문자열 str과 정수 n이 주어집니다.
+//str이 n번 반복된 문자열을 만들어 출력하는 코드를 작성해 보세요.
+//입력 #1
+//
+//string 5
+//출력 #1
+//
+//stringstringstringstringstring
+
+
+fun main(args: Array<String>) {
+    val input = readLine()!!.split(' ')
+    val s1 = input[0]
+    val a = input[1]!!.toInt()
+    for (i in 0 until a) {
+        print(s1)
+    }
+}
--- KotlinCoding/Kotlincoding2.kt
+++ KotlinCoding/Kotlincoding2.kt
@@ -8,10 +8,10 @@ package KotlinCoding
 //
 //a = 4
 //b = 5
-
-fun main(args: Array<String>) {
-    val (a, b) = readln().split(' ').map(String::toInt)
-
-    println("a = $a")
-    println("b = $b")
-}
+//
+//fun main(args: Array<String>) {
+//    val (a, b) = readln().split(' ').map(String::toInt)
+//
+//    println("a = $a")
+//    println("b = $b")
+//}
