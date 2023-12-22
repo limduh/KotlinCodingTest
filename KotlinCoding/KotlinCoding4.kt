8bf09b922dc2d259bd4c03ed720d87d6fb46a3c8
limduhyoung <limduh2@naver.com>
2023-12-22 23:06:49 +0900

    gogo

--- KotlinCoding/KotlinCoding4.kt
+++ KotlinCoding/KotlinCoding4.kt
@@ -1,12 +1,12 @@
 package KotlinCoding
 
 //영어 알파벳으로 이루어진 문자열 str이 주어집니다. 각 알파벳을 대문자는 소문자로 소문자는 대문자로 변환해서 출력하는 코드를 작성해 보세요
-
-fun main(args: Array<String>) {
-    println(
-        readLine()!!.map {
-            if (it.isUpperCase()) it.lowercaseChar()
-            else it.uppercaseChar()
-        }.joinToString("")
-    )
-}
+//
+//fun main(args: Array<String>) {
+//    println(
+//        readLine()!!.map {
+//            if (it.isUpperCase()) it.lowercaseChar()
+//            else it.uppercaseChar()
+//        }.joinToString("")
+//    )
+//}
---
+++ KotlinCoding/KotlinCoding5.kt
@@ -0,0 +1,10 @@
+package KotlinCoding
+
+class Solution {
+    fun solution(a: Int, b: Int): Long {
+        var answer = 0L
+        if (a > b) for (i in b..a) answer += i
+        else for (i in a..b) answer += i
+        return answer
+    }
+}
