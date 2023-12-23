4e77342590e11cfe8d7e30a83c5377f6cae3c385
limduhyoung <limduh2@naver.com>
2023-12-23 22:59:35 +0900

    gogo

--- KotlinCoding/KotlinCoding1.kt
+++ KotlinCoding/KotlinCoding1.kt
@@ -3,8 +3,10 @@ package KotlinCoding
 
 //문제 설명
 //문자열 str이 주어질 때, str을 출력하는 코드를 작성해 보세요.
-//
+
 //fun main(args: Array<String>) {
-//    val str = readLine()!!
+//    val str : String = readln()!!
 //    print(str)
 //}
+//
+//
---
+++ KotlinCoding/KotlinCoding6.kt
@@ -0,0 +1,25 @@
+package KotlinCoding
+//
+//입출력 예
+//예를들어 str이 "1234"이면 1234를 반환하고, "-1234"이면 -1234를 반환하면 됩니다.
+//str은 부호(+,-)와 숫자로만 구성되어 있고, 잘못된 값이 입력되는 경우는 없습니다.
+
+//프로그래머스 답
+//class Solution {
+//    fun solution(s: String): Int {
+//        return s.toInt()
+//    }
+//}
+
+// 내 답
+fun main() {
+
+//    val a : String = readln()
+//    val b : Int = a.toInt()
+//    print(b)
+
+
+fun solution(s: String): Int {
+    return s.toInt()
+}
+}
