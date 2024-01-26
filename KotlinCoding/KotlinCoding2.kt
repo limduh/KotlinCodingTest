9939d87c0efc6a1c0b4cbdccc45da04d76998140
limduhyoung <limduh2@naver.com>
2024-01-26 23:16:20 +0900

    readln().split(' ').map(toInt)틀림 저장

--- KotlinCoding/KotlinCoding2.kt
+++ KotlinCoding/KotlinCoding2.kt
@@ -9,11 +9,6 @@ package KotlinCoding
 //a = 4
 //b = 5
 //
-//fun main(args: Array<String>) {
-//    val (a, b) = readln().split(' ').map(String::toInt)
 //
-//    println("a = $a")
-//    println("b = $b")
-//}
 
-//map(String::toInt)틀림
+//readln().split(' ').map(String::toInt)틀림
