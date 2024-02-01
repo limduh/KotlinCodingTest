a9877028ff482f2ce638648a7332dab563ff4ddd
limduhyoung <limduh2@naver.com>
2024-02-01 23:24:17 +0900

    different UpperCase

--- KotlinCoding/KotlinCoding4.kt
+++ KotlinCoding/KotlinCoding4.kt
@@ -14,3 +14,13 @@ package KotlinCoding
 //
 //AbCdEfG
 
+fun main(args: Array<String>) {
+    val s1 = readLine()!!
+    for(i in s1.indices){
+        if(s1[i].isUpperCase()){
+            print(s1[i].toLowerCase())
+        }else{
+            print(s1[i].toUpperCase())
+        }
+    }
+}
