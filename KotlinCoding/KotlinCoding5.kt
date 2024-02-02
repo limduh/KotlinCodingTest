4537de16cab2a087cfea5f6d426ae5a73e08bc81
limduhyoung <limduh2@naver.com>
2024-02-02 23:15:12 +0900

    readln().split(' ').map(toInt)틀림 저장

--- KotlinCoding/KotlinCoding5.kt
+++ KotlinCoding/KotlinCoding5.kt
@@ -1,24 +1,14 @@
 package KotlinCoding
 //
+//문제 설명
+//다음과 같이 출력하도록 코드를 작성해 주세요.
 //
-//두 정수 a, b가 주어졌을 때 a와 b 사이에 속한 모든 정수의 합을 리턴하는 함수, solution을 완성하세요.
-//예를 들어 a = 3, b = 5인 경우, 3 + 4 + 5 = 12이므로 12를 리턴합니다.
+//출력 예시
 //
-//제한 조건
-//a와 b가 같은 경우는 둘 중 아무 수나 리턴하세요.
-//a와 b는 -10,000,000 이상 10,000,000 이하인 정수입니다.
-//a와 b의 대소관계는 정해져있지 않습니다.
-//입출력 예
-//a	b	return
-//3	5	12
-//3	3	3
-//5	3	12
+//  !@#$%^&*(\'"<>?:;
 
-class Solution5 {
-    fun solution(a: Int, b: Int): Long {
-        var answer = 0L
-        if (a > b) for (i in b..a) answer += i
-        else for (i in a..b) answer += i
-        return answer
-    }
+fun main(args: Array<String>) {
+
+    print("!@#\$%^&*(\\'\"<>?:;")
 }
+//
